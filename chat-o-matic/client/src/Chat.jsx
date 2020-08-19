1443c996906c163cf98333125c734d358ad1b283
sankhala96 <sankhalayash96@gmail.com>
2020-08-19 21:39:02 +0530

    Setting up Apollo GraphQL

--- chat-o-matic/client/src/Chat.jsx
+++ chat-o-matic/client/src/Chat.jsx
@@ -1,16 +1,134 @@
-import React from 'react';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import React, { useState } from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql, useMutation } from '@apollo/client';
+import { Container, Row, Col, FormInput, Button } from 'shards-react';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
   cache: new InMemoryCache()
 });
 
-const Chat = () => {
+const GET_MESSAGE = gql`
+query {
+    messages {
+        id
+        content
+        user
+    }
+}`;
+
+const POST_MESSAGE = gql`
+mutation ($user:String!, $content:String!) {
+    postMessage(user: $user, content: $content)
+}`
+
+const Messages = ({ user }) => {
+    const { data } = useQuery(GET_MESSAGE);
+
+    if(!data) {
+        return null;
+    }
+
     return (
-        <div>
-            I am chat window
-        </div>
+        <>
+            {data.messages.map(({ id, user: messageUser, content }) => {
+                return (
+                    <div
+                    style={{
+                        display:'flex',
+                        justifyContent: user === messageUser ? 'flex-end' : 'flex-start',
+                        paddingBottom: '1em'
+                    }}
+                >
+                    {user !== messageUser && (
+                        <div
+                            style={{
+                                height: 50,
+                                width: 50,
+                                marginRight: '0 .5em',
+                                border: '2px solid #e5e6ea',
+                                borderRadius: 25,
+                                textAlign: 'center',
+                                fontSize: '18pt',
+                                paddingTop: 5
+                            }}
+                        >
+                            {messageUser.slice(0,2).toUpperCase()}
+                        </div>
+                    )}
+                    <div
+                        style={{
+                            background: user === messageUser ? '#58bf56' : '#e5e6ea',
+                            color: user === messageUser ? 'white' : 'black',
+                            padding: '1em',
+                            borderRadius: '1em',
+                            maxWidth: '60%'
+                        }}
+                    >
+                        {content}
+                    </div>
+                </div>
+                )
+            })}
+        </>
+    );
+}
+
+const Chat = () => {
+    const [state, setState] = useState({
+        user: 'jack',
+        content: ''
+    });
+
+    const [postMessage] = useMutation(POST_MESSAGE);
+
+    const onSend = () => {
+        if(state.content.length > 0) {
+            postMessage({
+                variables: state,
+            })
+        }
+
+        setState({
+            ...state,
+            content: '',
+        })
+    }
+    return (
+        <Container>
+            <Messages user={state.user} />
+            <Row>
+                <Col xs={2} style={{ padding: 0 }}>
+                    <FormInput
+                        label="user"
+                        value={state.user}
+                        onChange={(e) => setState({
+                            ...state,
+                            user: e.target.value
+                        })}
+                    />
+                </Col>
+                <Col xs={8}>
+                    <FormInput
+                        label="user"
+                        value={state.content}
+                        onChange={(e) => setState({
+                            ...state,
+                            content: e.target.value
+                        })}
+                        onKeyUp={(e) => {
+                            if(e.keyCode === 13) {
+                                onSend();
+                            }
+                        }}
+                    />
+                </Col>
+                <Col xs={2} style={{ padding: 0 }}>
+                    <Button onClick={() => onSend()}>
+                        Send
+                    </Button>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
