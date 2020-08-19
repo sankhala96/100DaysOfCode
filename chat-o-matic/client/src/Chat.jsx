ac59e947d351543d06e9918607c470119968412b
sankhala96 <sankhalayash96@gmail.com>
2020-08-19 20:59:41 +0530

    Setting up Client

---
+++ chat-o-matic/client/src/Chat.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache()
+});
+
+const Chat = () => {
+    return (
+        <div>
+            I am chat window
+        </div>
+    )
+}
+
+export default () => (
+    <ApolloProvider client={client}>
+        <Chat />
+    </ApolloProvider>
+)
