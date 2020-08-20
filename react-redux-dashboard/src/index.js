d35d84dbb4b60f1d4a4d5bef20b6f59c0bbfcfc9
sankhala96 <sankhalayash96@gmail.com>
2020-08-21 00:31:21 +0530

    setting up redux

--- react-redux-dashboard/src/index.js
+++ react-redux-dashboard/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import store from './store';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
